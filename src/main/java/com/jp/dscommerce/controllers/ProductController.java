d51dbafa4ea1ed2f05ad2be8993a96638926dde5
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-10 18:19:16 -0300

    first API testing repository (returning object name)

--- src/main/java/com/jp/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jp/dscommerce/controllers/ProductController.java
@@ -1,14 +1,22 @@
 package com.jp.dscommerce.controllers;
 
+import com.jp.dscommerce.entities.Product;
+import com.jp.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController //possibilita resposta da classe na web
 @RequestMapping(value = "/products")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository; //injetando a dependência de ProductRepository
     @GetMapping
     public String testeAPI() {
-        return "Teste";
+        Optional<Product> product = repository.findById(1L);
+        return product.get().getName();
     }
 }
---
+++ src/main/java/com/jp/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.jp.dscommerce.repositories;
+
+import com.jp.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+
+}
