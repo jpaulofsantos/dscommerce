cf239ada17430d87b1be09f0c683ec8ddd319ad5
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-11 21:30:09 -0300

    GET product (pageable)

--- src/main/java/com/jp/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jp/dscommerce/controllers/ProductController.java
@@ -1,16 +1,17 @@
 package com.jp.dscommerce.controllers;
 
 import com.jp.dscommerce.dto.ProductDTO;
-import com.jp.dscommerce.entities.Product;
-import com.jp.dscommerce.repositories.ProductRepository;
 import com.jp.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @RestController //possibilita resposta da classe na web
 @RequestMapping(value = "/products")
@@ -18,7 +19,12 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     @GetMapping(value = "/{id}")
-    public ProductDTO findBtId(@PathVariable Long id) { //@PathVariable id é o mesmo id da rota do @GetMapping {id}
+    public ProductDTO findById(@PathVariable Long id) { //@PathVariable id é o mesmo id da rota do @GetMapping {id}
         return productService.findById(id);
+    }
+
+    @GetMapping //http://localhost:8080/products?size=12&page=1&sort=name,desc (passando um query param de 12 resultados por pagina e filtrando pela pagina 2 e ordenando pelo name em order descrescente)
+    public Page<ProductDTO> findAll(Pageable pageable){ //retorno com paginação. Caso queira retornar todos os dados basta remover o parametro do metodo e alterar para List ao inves de Page
+        return productService.findAll(pageable);
     }
 }
--- src/main/java/com/jp/dscommerce/services/ProductService.java
+++ src/main/java/com/jp/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.jp.dscommerce.dto.ProductDTO;
 import com.jp.dscommerce.entities.Product;
 import com.jp.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +21,11 @@ public class ProductService { //service devolve um DTO para o Controller
         Optional<Product> result = productRepository.findById(id); //busca no banco de dados o objeto passando o ID
         Product product = result.get(); //get no objeto
         return new ProductDTO(product); //cria um novo productDTO passando o product recuperado do banco
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> resultList = productRepository.findAll(pageable); //busca no banco de dados a lista com todos os registros
+        return resultList.map(x-> new ProductDTO(x)); //converte a lista em um ProductDTO
     }
 }
