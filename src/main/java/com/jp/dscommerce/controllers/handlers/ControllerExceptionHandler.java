ed9d249062dfaa416f4171331eeb20ed8c8c09c0
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-15 17:22:04 -0300

    add FieldMessage / Validation error to handle validation error list

--- src/main/java/com/jp/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/jp/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.jp.dscommerce.controllers.handlers;
 
 import com.jp.dscommerce.dto.CustomError;
+import com.jp.dscommerce.dto.ValidationError;
 import com.jp.dscommerce.services.exceptions.DataBaseException;
 import com.jp.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerExceptionHandler { //classe para controle das exception (envio de msg customizada para o client)
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
@@ -23,6 +27,17 @@ public class ControllerExceptionHandler { //classe para controle das exception (
     public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()){ // percorrendo a lista contendo os erros da exceção
+            err.addError(f.getField(), f.getDefaultMessage()); //pega as informações do dto
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
